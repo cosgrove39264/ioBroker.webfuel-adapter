97e4720071307abd421e4ad591366682077776be
Marcel Schmidt (Home) <derprogger@gmail.com>
2022-02-15 12:05:45 +0100

    Initial commit

---
+++ admin/words.js
@@ -0,0 +1,41 @@
+/*global systemDictionary:true */
+"use strict";
+
+systemDictionary = {
+	"webfuel-adapter adapter settings": {
+		"en": "Adapter settings for webfuel-adapter",
+		"de": "Adaptereinstellungen für webfuel-adapter",
+		"ru": "Настройки адаптера для webfuel-adapter",
+		"pt": "Configurações do adaptador para webfuel-adapter",
+		"nl": "Adapterinstellingen voor webfuel-adapter",
+		"fr": "Paramètres d'adaptateur pour webfuel-adapter",
+		"it": "Impostazioni dell'adattatore per webfuel-adapter",
+		"es": "Ajustes del adaptador para webfuel-adapter",
+		"pl": "Ustawienia adaptera dla webfuel-adapter",
+		"zh-cn": "webfuel-adapter的适配器设置"
+	},
+	"option1": {
+		"en": "option1",
+		"de": "Option 1",
+		"ru": "Опция 1",
+		"pt": "Opção 1",
+		"nl": "Optie 1",
+		"fr": "Option 1",
+		"it": "opzione 1",
+		"es": "Opción 1",
+		"pl": "opcja 1",
+		"zh-cn": "选项1"
+	},
+	"option2": {
+		"en": "option2",
+		"de": "Option 2",
+		"ru": "вариант 2",
+		"pt": "opção 2",
+		"nl": "Optie 2",
+		"fr": "Option 2",
+		"it": "opzione 2",
+		"es": "opcion 2",
+		"pl": "Opcja 2",
+		"zh-cn": "选项2"
+	}
+};
